e9fabbce39ac208780fe19eefbf194ec54e452be
alfred <cabatoalfred725@gmail.com>
2025-08-14 19:59:39 +0800

    deleting index

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,8 @@ export default defineNuxtConfig({
 		plugins: [vuetify()],
 	},
 
+	
+
 	modules: ['@nuxtjs/apollo', ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate'] }]],
 	compatibilityDate: '2024-11-11',
 })
